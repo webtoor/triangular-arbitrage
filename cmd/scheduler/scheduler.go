4380950a69c844a713ee4f1497b9b95be6e0bd4e
webtoor <fityanalishi@gmail.com>
2025-02-17 23:00:52 +0700

    feat: scheduler with stop when error

--- cmd/scheduler/scheduler.go
+++ cmd/scheduler/scheduler.go
@@ -2,18 +2,31 @@ package scheduler
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/go-co-op/gocron"
 	"github.com/webtoor/triangular-arbitrage/internal/appctx"
 	"github.com/webtoor/triangular-arbitrage/internal/triangular"
+	"github.com/webtoor/triangular-arbitrage/pkg/logger"
 )
 
 func Start(cfg *appctx.Config, t triangular.Resolverer) {
+	var (
+		lf = logger.NewFields(
+			logger.EventName("scheduler.start"),
+		)
+	)
+
+	logger.Info("starting triangular arbitrage", lf...)
 	s := gocron.NewScheduler(time.Local)
 
 	s.Every(cfg.Triangular.Interval).Second().Do(func() {
-		t.Start(context.Background())
+		err := t.Start(context.Background())
+		if err != nil {
+			logger.Error(fmt.Sprintf("scheduler stopped cause: %v", err), lf...)
+			s.Stop()
+		}
 	})
 
 	s.StartBlocking()
